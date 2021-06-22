5b506d71d57c004b6269bcac1934c64ea0d47032
Mavludin <imevlud@gmail.com>
2021-06-22 17:02:51 +0300

    new branch

--- src/index.js
+++ src/index.js
@@ -6,12 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { globalStore } from './store';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot( document.getElementById('root'));
+
+root.render(
   <Provider store={globalStore}>
     <App />
-  </Provider>,
-  document.getElementById('root')
-);
+  </Provider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
